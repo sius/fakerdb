ebee71834cb5fc07c7c9f4317d883d4e6301d12a
sius <sius@gmx.net>
2018-10-30 00:38:01 +0100

    update index

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-let { Chance } = require('chance')
-  , faker = require('faker')
-  , jsf = require('json-schema-faker')
-  , { replay, jsft, insert, progress } = require('./streams');
 
-jsf.extend('faker', () => faker);
-jsf.extend('chance', () => new Chance());
+const { replay, jsft, insert, progress } = require('./streams');
 
 module.exports = function(db, schema, options = null, cb = null ) {
   let _db = db;
-  let _opts = options || { replay: 1, insert: { blockSize: 100 } };
   if (!db) {
     const Datasource = require('nedb');
     _db = new Datasource({ filename: 'faker.db', autoload: true })
   }
-  
+  let _opts = options || { replay: 1, insert: { blockSize: 1 } };
+  let jsf = _opts.jsft;
+  if (!jsf) {
+    jsf = require('json-schema-faker')
+    let { Chance } = require('chance')
+      , faker = require('faker');
+    jsf.extend('faker', () => faker);
+    jsf.extend('chance', () => new Chance());
+  }
+
   replay(schema, _opts)
     .pipe(jsft(jsf))
     .pipe(insert(_db, _opts))
