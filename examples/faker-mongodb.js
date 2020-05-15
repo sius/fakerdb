465872f88d4cb1f3eec0243e2bcb506fe8d48f5e
sius <sius@gmx.net>
2020-05-15 18:30:33 +0200

    update

--- examples/faker-mongodb.js
+++ examples/faker-mongodb.js
@@ -7,7 +7,7 @@ const REPLAY = 1000;
 const SCHEMA = path.join(__dirname, './schema/person.json');
 const OPTS = { replay: REPLAY, insert: { blockSize: 1000 } };
 
-MongoClient.connect('mongodb://localhost:27017/fakerdb', { useNewUrlParser: true }, (err, client) => {
+MongoClient.connect('mongodb://localhost:27017/fakerdb', { useUnifiedTopology: true, useNewUrlParser: true }, (err, client) => {
   if (err) {
     return console.log(err)
   }
